ac02155973334afac2dbf3ab045cf495ac61102e
morgnism <morgan@border.dev>
2022-10-25 23:30:54 -0400

    updated portable text component to handle undefined value

--- web/src/components/PortableText/SummaryPortableText/SummaryPortableText.tsx
+++ web/src/components/PortableText/SummaryPortableText/SummaryPortableText.tsx
@@ -1,15 +1,25 @@
 import { PortableText } from '@portabletext/react';
 import { SummaryPortableText } from '../../../../../studio/schema';
 
-const SummaryPortableText = ({ summary }: { summary: SummaryPortableText }) => (
-  <PortableText
-    value={summary}
-    components={{
-      block: {
-        span: ({ value }) => <>{value}</>,
-      },
-    }}
-  />
-);
+const SummaryPortableText = ({
+  summary,
+}: {
+  summary?: SummaryPortableText;
+}) => {
+  if (!summary) {
+    return null;
+  }
+
+  return (
+    <PortableText
+      value={summary}
+      components={{
+        block: {
+          span: ({ value }) => <>{value}</>,
+        },
+      }}
+    />
+  );
+};
 
 export default SummaryPortableText;
